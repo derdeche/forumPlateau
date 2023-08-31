821057229223be7aed871c6a6ce84aa90788c22f
derdeche <foued.derdeche@gmail.com>
2023-08-31 14:18:32 +0200

    modif link affichage

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -6,5 +6,6 @@ $categories=$result["data"]["categories"];
 
 foreach($categories as $categorie)
 {?>
-   <p> <?= $categorie->getNameCategorie() ?></p>;
-<?php}?>
+   <p> <a href="index.php?ctrl=forum&action=ListTopics&id="><?$this?><?= $categorie->getCategoryName() ?></a></p>;
+
+<?php } ?>
