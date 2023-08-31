1b469b7f634f629e92417fcbc876aea1df6414e2
derdeche <foued.derdeche@gmail.com>
2023-08-31 16:53:29 +0200

    add use model categoryManager

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -8,6 +8,7 @@
     use Model\Managers\UserManager;
     use Model\Managers\TopicManager;
     use Model\Managers\PostManager;
+    use Model\Managers\CategoryManager;
     
     class HomeController extends AbstractController implements ControllerInterface{
 
