0e2df1b4680a80ec31d9d2ddb33dd2dbccbc1789
derdeche <foued.derdeche@gmail.com>
2023-09-01 16:41:03 +0200

    add function addCategory

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -50,7 +50,84 @@
          
          }
 
+ 
+
+    public function addCategory() {
+    
+        $categoryManager = new CategoryManager();
+        $user = Session::getUser();
+   
+        // Vérifiez si les données requises existent
+            
+	
+		if (isset($_POST["submit"])) {
+			
+			$category= filter_input(INPUT_POST, "categoryName", FILTER_SANITIZE_SPECIAL_CHARS);
+        }                 
+             
+                
+                       
+        // Préparez la requête SQL avec un paramètre de liaison
+                
+                $
+        
+               
+        $sql = "INSERT INTO " . $this->category . " (categoryName) VALUES (:categoryName)";
+        
+      
+               
+        try {
+            // Utilisez la méthode d'insertion du gestionnaire de catégories
+            $result = $categoryManager->insertCategory($sql, $categoryName);
+        
+                    
+        
+                   
+        // Vérifiez si l'insertion s'est bien déroulée
+                    if ($result) {
+                        
+                       
+        // Redirigez l'utilisateur vers une page de succès ou effectuez une autre action
+                        
+                       
+        // Vous pouvez rediriger en utilisant header('Location: ...');
+                    } else {
+                        echo "Erreur lors de l'insertion de la catégorie.";
+                    }
+                } catch (\PDOException $e) {
+                    echo $e->getMessage();
+                    die();
+                }
+            } 
+                }
+            
+        
+                
+         {
+                
+               
+        echo "Données de catégorie manquantes.";
+            }
         
 
 
-    }
+        
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
