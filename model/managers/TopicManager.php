10dbf04200ab09f54b9e9bfcca7d70a19fe00454
derdeche <foued.derdeche@gmail.com>
2023-09-04 06:22:39 +0200

    add function suppTopic

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -33,5 +33,13 @@
         );
     }
 
+    public function suppTopic($id)
+    {
+        $sql = "DELETE FROM ".$this->tableName."
+            WHERE id_".$this->tableName."=:id";
+
+        DAO::delete($sql, ['id' => $id]);
+    }
+
 
     }
