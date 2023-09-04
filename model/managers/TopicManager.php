b2e4427a973dd606691b97e25860adfeed9e4f4a
derdeche <foued.derdeche@gmail.com>
2023-09-04 05:21:49 +0200

    add function topicByCategory

--- model/managers/TopicManager.php
+++ model/managers/TopicManager.php
@@ -15,5 +15,23 @@
             parent::connect();
         }
 
+    public function topicByCategory($id)
+    {
+
+        $sql = "SELECT id_topic, topicName, topicDate, topic.user_id, 
+                            COUNT(post.topic_id) as nbPosts
+                    FROM " . $this->tableName . " topic
+                    INNER JOIN post ON post.topic_id = topic.id_" . $this->tableName . "
+                    WHERE topic.category_id = :id
+                    GROUP BY topic.id_" . $this->tableName . "
+                    ORDER BY topic.topicDate DESC";
+
+
+        return $this->getMultipleResults(
+            DAO::select($sql, ['id' => $id]),
+            $this->className
+        );
+    }
+
 
     }
