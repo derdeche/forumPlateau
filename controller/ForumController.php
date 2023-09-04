b1f3d91af830d3625545867e463f32dfb49b3f17
derdeche <foued.derdeche@gmail.com>
2023-09-04 05:21:11 +0200

    add function listtopicba category

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -50,64 +50,90 @@
          
          }
 
- 
-
-    public function addCategory() {
+        public function listTopicsByCategory($id){
     
-        $categoryManager = new CategoryManager();
-        $user = Session::getUser();
+            $topicManager = new TopicManager();
+            $categoryManager = new CategoryManager();
+    
+            return [
+                "view" => VIEW_DIR . "forum/listTopicsByCategory.php",
+                "data" => [
+                    "topics" => $topicManager->topicByCategory($id),
+                    "categories" => $categoryManager->findOneById($id)
+                ]
+            ];
+        }
+
+        public function listPostsByTopic($id){
+    
+            $postManager = new PostManager();
+            $topicManager = new TopicManager();
+    
+            return [
+                "view" => VIEW_DIR . "forum/listPostsByTopic.php",
+                "data" => [
+                    "posts" => $postManager->postByTopic($id),
+                    "topic" => $topicManager->findOneById($id)
+                ]
+            ];
+        }
+
+    // public function addCategory() {
+    
+    //     $categoryManager = new CategoryManager();
+    //     $user = Session::getUser();
    
-        // Vérifiez si les données requises existent
+    //     // Vérifiez si les données requises existent
             
 	
-		if (isset($_POST["submit"])) {
+	// 	if (isset($_POST["submit"])) {
 			
-			$category= filter_input(INPUT_POST, "categoryName", FILTER_SANITIZE_SPECIAL_CHARS);
-        }                 
+	// 		$category= filter_input(INPUT_POST, "categoryName", FILTER_SANITIZE_SPECIAL_CHARS);
+    //     }                 
              
                 
                        
-        // Préparez la requête SQL avec un paramètre de liaison
+    //     // Préparez la requête SQL avec un paramètre de liaison
                 
-                $
+    //             $
         
                
-        $sql = "INSERT INTO " . $this->category . " (categoryName) VALUES (:categoryName)";
+    //     $sql = "INSERT INTO " . $this->category . " (categoryName) VALUES (:categoryName)";
         
       
                
-        try {
-            // Utilisez la méthode d'insertion du gestionnaire de catégories
-            $result = $categoryManager->insertCategory($sql, $categoryName);
+    //     try {
+    //         // Utilisez la méthode d'insertion du gestionnaire de catégories
+    //         $result = $categoryManager->insertCategory($sql, $categoryName);
         
                     
         
                    
-        // Vérifiez si l'insertion s'est bien déroulée
-                    if ($result) {
+    //     // Vérifiez si l'insertion s'est bien déroulée
+    //                 if ($result) {
                         
                        
-        // Redirigez l'utilisateur vers une page de succès ou effectuez une autre action
+    //     // Redirigez l'utilisateur vers une page de succès ou effectuez une autre action
                         
                        
-        // Vous pouvez rediriger en utilisant header('Location: ...');
-                    } else {
-                        echo "Erreur lors de l'insertion de la catégorie.";
-                    }
-                } catch (\PDOException $e) {
-                    echo $e->getMessage();
-                    die();
-                }
-            } 
-                }
+    //     // Vous pouvez rediriger en utilisant header('Location: ...');
+    //                 } else {
+    //                     echo "Erreur lors de l'insertion de la catégorie.";
+    //                 }
+    //             } catch (\PDOException $e) {
+    //                 echo $e->getMessage();
+    //                 die();
+    //             }
+    //         } 
+    //             }
             
         
                 
-         {
+    //      {
                 
                
-        echo "Données de catégorie manquantes.";
-            }
+    //     echo "Données de catégorie manquantes.";
+    //         }
         
 
 
@@ -120,7 +146,7 @@
         
 
 
-
+    }
 
 
 
