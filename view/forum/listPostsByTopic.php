2c739f2b696f94403d055bdd06e85548aa388305
derdeche <foued.derdeche@gmail.com>
2023-09-04 17:01:08 +0200

    add form post

--- view/forum/listPostsByTopic.php
+++ view/forum/listPostsByTopic.php
@@ -16,4 +16,13 @@ foreach($posts as $post ){
     ?>
     <p><?=$post->getText()?> <?=$post->getDatePost()?> </p>
     <?php
-}
+}?>
+
+<p>ajouter un post</p>
+<form action="index.php?ctrl=forum&action=addPost" method= POST>
+    <label for="text">text</label>
+    <input type="text" name="text" id="text">
+    <button type="submit" name="submit">Ajouter</button>
+</form>
+
+
