a3a55bbb614fbbba466d28fed6fe5fbf270d10eb
derdeche <foued.derdeche@gmail.com>
2023-09-06 16:52:43 +0200

    modif link

--- view/forum/listTopicsByCategory.php
+++ view/forum/listTopicsByCategory.php
@@ -9,27 +9,28 @@ $categories = $result["data"]["categories"];
 <h1>liste sujets</h1>
 
 <?php
-foreach($topics as $topic ){
+foreach($topics as $topic ){ ?>
 
-    ?>
-    <p><a href="index.php?ctrl=forum&action=ListPostsByTopic&id=<?= $topic->getId() ?>"><?=$topic->getTopicName()?></a></p>
-    <a><?=$topic->getTopicDate()?></a> 
+    
+    <p><a href="index.php?ctrl=forum&action=ListPostsByTopic&id=<?= $topic->getId() ?>"><?=$topic->getTopicName()?></a><a><?=$topic->getTopicDate()?></a> 
+    <form action="index.php?ctrl=forum&action=deleteTopic&id=<?= $id ?>" method = POST >
+      <button type ="submit" name= "submit">Supprimer</button>
+   </form></p>
+    
     
     <?php } ?>
 
 <p>Ajouter un Sujet</p>
 
 
-<form action="index.php?ctrl=forum&action=addTopic&id=<?= $id ?>" method = "POST"  >
+<form action="index.php?ctrl=forum&action=addTopic&id=<?= $id ?>" method = POST >
    <label ></label>
    <input type="text" name="topicName" >
    <button type ="submit" name= "submit">Ajouter</button>
 </form>
 
-<p>Supprimer un Sujet</p>
-<form action="index.php?ctrl=forum&action=deleteTopic&id=<?= $id ?>" method = "POST"  >
+
   
-   <button type ="delete" name= "delete">Supprimer</button>
-</form>
+   
 
 
