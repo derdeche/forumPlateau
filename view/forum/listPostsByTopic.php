8d83c3d0e33e6eeced334967c2a8ada8f452c8a6
derdeche <foued.derdeche@gmail.com>
2023-09-06 16:52:30 +0200

    modif link

--- view/forum/listPostsByTopic.php
+++ view/forum/listPostsByTopic.php
@@ -4,7 +4,7 @@ $posts = $result["data"]['posts'];
 $topics = $result["data"]['topics'];
 // $topicId = $_GET['id'];
 
-// $categoryId = $_GET['id'];
+$categoryId = $_GET['id'];
     
 ?>
 
@@ -14,10 +14,14 @@ $topics = $result["data"]['topics'];
 foreach($posts as $post ){
 
     ?>
-    <p><?=$post->getText()?> <?=$post->getDatePost()?> </p>
-    <?php
-}?>
-
+    <p><?=$post->getText()?> <?=$post->getDatePost()?>
+    <form action="index.php?ctrl=forum&action=deletePost&id=<?= $post->getId() ?>" method = POST  >
+        <button type ="submit" name= "submit">Supprimer</button>
+    </form></p>
+    <?php } ?>
+      
+       
+  
 <p>ajouter un post</p>
 
 <form action="index.php?ctrl=forum&action=addPost&id=<?= $id ?>" method= POST>
@@ -26,11 +30,6 @@ foreach($posts as $post ){
     <button type="submit" name="submit">Ajouter</button>
 </form>
 
-<p>Supprimer un post</p>
 
-<form action="index.php?ctrl=forum&action=deletePost&id=<?= $id ?>" method = "POST"  >
-  
-   <button type ="delete" name= "delete">Supprimer</button>
-</form>
 
 
