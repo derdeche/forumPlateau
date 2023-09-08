08dcc1725b70cddea1b270c73cf59bc3c6496e8c
derdeche <foued.derdeche@gmail.com>
2023-09-08 16:19:39 +0200

    modif name

--- view/security/register.php
+++ view/security/register.php
@@ -2,16 +2,16 @@
 <p>S'inscrire</p>
 
 
-<form action="index.php?ctrl=security&action=register" method = POST>
+<form action="index.php?ctrl=Security&action=register" method = POST>
 
     <label >Pseudo</label>
-    <input type ="text" name="username" required>
+    <input type ="text" name="pseudo" required>
 
     <label >Email</label>
     <input type ="email" name="email" required placeholder="....@....">
     
     <label >Mot de Passe</label>
-    <input type="password" name="password1"  required placeholder="*****">
+    <input type="password" name="password"  required placeholder="*****">
 
     <label >Confirmer du mot de Passe</label>
     <input type="password" name="password2"  required placeholder="*****">
