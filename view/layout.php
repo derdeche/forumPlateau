5fb334e5bd1219cb54ecae974b5c9c61e842e57e
derdeche <foued.derdeche@gmail.com>
2023-09-08 16:19:14 +0200

    modif link

--- view/layout.php
+++ view/layout.php
@@ -41,7 +41,7 @@
                         else{
                             ?>
                             <a href="view/security/login.php">Connexion</a>
-                            <a href="view/security/register.php">Inscription</a>
+                            <a href="index.php?ctrl=security&action=toRegister">Inscription</a>
                             <!-- <a href="index.php?ctrl=forum&action=listTopics">La liste des Catégories</a> -->
                             <a href="index.php?ctrl=forum&action=ListCategories">La liste des Catégories</a>
                         <?php
