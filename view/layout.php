5e138112bb0740201e1c4f041178891b94ff0b58
derdeche <foued.derdeche@gmail.com>
2023-09-14 16:45:59 +0200

    add link

--- view/layout.php
+++ view/layout.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="public/css/style.css">
     <title>FORUM</title>
 </head>
 <body>
@@ -38,6 +38,7 @@
                             <a href="/security/viewProfile.php"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
                             <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
                             <a href="index.php?ctrl=forum&action=ListCategories">La liste des Catégories</a>
+                            <a href="index.php?ctrl=home&action=users">Voir la liste des gens</a>
                             <?php
                         }
                         else{
